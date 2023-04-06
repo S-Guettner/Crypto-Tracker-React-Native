aa640476c06ac1166718bf60d6d490077a499d30
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-06 13:37:37 +0200

    setting up tailwind (NativeWind)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const Hello = () => {
+
+
+	return (
+    
+
+		<SafeAreaView >
+			<View >
+				<View >
+					<Text className='text-red-200'>
+						Hello Tailwind !!!!!!
+					</Text>
+				</View>
+			</View>
+		</SafeAreaView>
+
+    
+	);
+};
+
+export default Hello;
